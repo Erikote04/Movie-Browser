27eb7324a63600cb7f9cb27014ad5797de8c7071
Erikote <erikote2004@icloud.com>
2023-12-06 14:32:46 +0100

    Iterating over response array and showing info about movies

--- src/Input.svelte
+++ src/Input.svelte
@@ -26,7 +26,16 @@
     <p>Loading...</p>
 {:else}
     {#if response.length > 0}
-        <p>We have {response.length} movies for your search</p>
+        <div>
+            <p>We have {response.length} movies for your search</p>
+            {#each response as movie}
+                <article>
+                    <img src={movie.Poster} alt={movie.Title}>
+                    <h3>{movie.Title}</h3>
+                    <span>{movie.Year}</span>
+                </article>
+            {/each}
+        </div>
     {:else}
         <p>No results found</p>
     {/if}
