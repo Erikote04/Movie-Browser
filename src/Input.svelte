05fc92906ebe10ead0d1f40594fca058aaab06ef
Erikote <erikote2004@icloud.com>
2023-12-06 14:00:41 +0100

    Input (search) component. Reactive block and fetching data

---
+++ src/Input.svelte
@@ -0,0 +1,14 @@
+<script>
+    let value = "";
+    let response = [];
+
+    const handleInput = (e) => value = e.target.value;
+
+    $: if (value.length > 2){
+        fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
+        .then(res => res.json())
+        .then((apiResponse) => response = apiResponse.Search);
+    }
+</script>
+
+<input value on:input={handleInput}>
