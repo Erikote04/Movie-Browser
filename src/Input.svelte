e1924301d190cfb2fdb8831d4a432978325c979d
Erikote <erikote2004@icloud.com>
2023-12-06 14:11:00 +0100

    Conditional render (bad practice)

--- src/Input.svelte
+++ src/Input.svelte
@@ -7,8 +7,18 @@
     $: if (value.length > 2){
         fetch(`https://www.omdbapi.com/?s=${value}&apikey=422350ff`)
         .then(res => res.json())
-        .then((apiResponse) => response = apiResponse.Search);
+        .then((apiResponse) => response = apiResponse.Search || []);
     }
 </script>
 
-<input value on:input={handleInput}>
+<input 
+    placeholder="Search movies..." 
+    value 
+    on:input={handleInput}
+>
+
+{
+    response.length > 0
+        ? `We have ${response.length} movies for your search`
+        : "No results found"
+}
